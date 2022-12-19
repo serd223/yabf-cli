a31b651cd3c7be7c4db7e0c4b90dc00ae3436122
serd <patatd@outlook.com.tr>
2022-12-19 21:49:02 +0300

    Run rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use bf_debugger::BfDebugger;
-use yabf::{Program, BfInstance};
+use yabf::{BfInstance, Program};
 
 mod bf_debugger;
 
 fn main() {
     // Temporary debug
     let program = Program::from(">+++++++++++[<+++++++++++>-]<>>++++[<++++>-]<>++++++<<");
-    let program2 = Program::from(">>>>>>>>>>>>>>>>>>>>>>>>+++++++++++[<+++++++++++>-]<>>++++[<++++>-]<>++++++<<");
-    
+    let program2 = Program::from(
+        ">>>>>>>>>>>>>>>>>>>>>>>>+++++++++++[<+++++++++++>-]<>>++++[<++++>-]<>++++++<<",
+    );
+
     let mut bf: BfInstance<256> = BfInstance::from(program);
     bf.run();
-    
     let mut bf2: BfInstance<256> = BfInstance::from(program2);
     bf2.run();
-    
+
     let mut debugger = BfDebugger {
         bf: bf,
         ..Default::default()
@@ -22,6 +23,7 @@ fn main() {
         bf: bf2,
         ..Default::default()
     };
+
     for p in 5..14 {
         debugger.cfg.dump_padding = p;
         debugger2.cfg.dump_padding = p;
@@ -29,7 +31,7 @@ fn main() {
         println!("Len: 11");
         println!("{}", debugger.dump_mem(11));
         println!("{}", debugger2.dump_mem(11));
-        
+
         println!("Len: 10");
         println!("{}", debugger.dump_mem(10));
         println!("{}", debugger2.dump_mem(10));
